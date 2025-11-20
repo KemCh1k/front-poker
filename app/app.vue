88715b301329f1fd5f6105d561bec03cc7f49946
glebb <glebba7771@gmail.com>
2025-11-20 16:39:09 +0300

    start-change

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">mwlkdc</div>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped>
+.header {
+  @apply bg-green-950 p-32;
+}
+</style>
